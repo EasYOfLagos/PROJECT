38f149cb3a773b9dde391b038354a97bd9c2b1cc
easyoflagos <isiakaibiyemi83@gmail.com>
2024-04-29 13:43:38 +0100

    working on a new project

---
+++ design.js
@@ -0,0 +1,8 @@
+let menus = document.querySelector("navbar");
+let menusbtn = document.querySelector("fa-slack")
+// let closemenu = document.querySelector("
+
+
+menusbtn.addEventListener(`click`,function(){
+    menus.classList.add("active");
+})
